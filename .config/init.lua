fd4379ae95719634091cd2a55e614df6ffb32b94
Mohit Singh Panwar <95737720+MohitSingh65@users.noreply.github.com>
2026-04-30 00:20:09 +0530

    new nvim config

---
+++ .config/init.lua
@@ -0,0 +1,34 @@
+require("core.options")
+require("core.keymaps")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.deprecate = function() end
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.autopair"),
+	require("plugins.smooth-scrolling"),
+	require("plugins.theme-selector"),
+	require("plugins.toggleterm"),
+	require("plugins.harpoon"),
+	require("plugins.colors"),
+	require("plugins.none-ls")
+})
